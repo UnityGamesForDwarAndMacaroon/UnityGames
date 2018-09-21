57bb36235fd4ec57bb27bb773fda6f2909ae2e6f
dwarzdiks <mushroombrony+git@gmail.com>
2018-09-21 23:12:01 +0900

    テストステージ作成とCameraスクリプトとか追加

---
+++ Assets/Script/CameraScripts/CameraPathController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPathController : MonoBehaviour {
+    public int time = 100;
+    public string PathName = "New Path 1";
+
+    void Start()
+    {
+        iTween.MoveTo(this.gameObject, iTween.Hash(
+            "path", iTweenPath.GetPath(PathName),
+            "time", time,
+            "easeType", iTween.EaseType.linear,
+            "orienttopath", true));
+    }
+}
